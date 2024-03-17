6c0a872b785b74020f2d0cec2e3914536b5211de
PObloj <PObloj@PIOTRO-NTB>
2024-03-17 17:46:18 +0100

    Zadanie domowe 6

---
+++ ChallengeApp2/ChallengeApp2.Test/EmployeeTest.cs
@@ -0,0 +1,57 @@
+namespace ChallengeApp2.Test
+{
+    public class Test
+    {
+        
+        [Test]
+        public void EmployeesPointAdditionTest()
+        {
+            
+        
+              //arrange
+
+                var emps = new Employee("Piotr", "Nowak", "45cgbgb");
+
+                emps.AddRtp(5);
+                emps.AddRtp(6);
+                emps.AddRtp(10);
+                emps.AddRtp(4);
+
+                //act
+
+                var result = emps.Result;
+
+
+
+                //assert
+
+                Assert.AreEqual(16, result);
+        }
+
+        [Test]
+        public void EmployeesPointAdditionTest2()
+        {
+
+
+            //arrange
+
+            var emps = new Employee("Piotr", "Nowak", "45cgbgb");
+
+            emps.AddRtp(0);
+            emps.AddRtp(6);
+            emps.AddRtp(10);
+            emps.AddRtp(-5);
+
+            //act
+
+            var result = emps.Result;
+
+
+
+            //assert
+
+            Assert.AreEqual(11, result);
+        }
+
+    }
+}
