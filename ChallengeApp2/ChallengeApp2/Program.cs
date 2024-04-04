21e865d70270e96134a0f1fbbbe83add7e1f914e
PObloj <PObloj@PIOTRO-NTB>
2024-04-04 19:15:26 +0200

    Do dnia   9

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -2,63 +2,13 @@
 
 using ChallengeApp2;
 
-Employee emp1 = new Employee("Piotr", "Nowak", "51");
-Employee emp2 = new Employee("Zuza", "Kowalska", "22");
-Employee emp3 = new Employee("Andrzej", "Czaszkowski", "58");
+var employee = new Employee("Piotr", "Obłój");
+employee.AddGrade(5);
+employee.AddGrade(6);
+employee.AddGrade(10);
 
+var statistics = employee.GetStatistics();
+Console.WriteLine($"Wartość Max: {statistics.Max}");
+Console.WriteLine($"Średnia wartość: {statistics.Average:N2}");
+Console.WriteLine($"Wartość Min: {statistics.Min}");
 
-// pracownicy
-emp1.AddRtp(2);
-emp1.AddRtp(5);
-emp1.AddRtp(4);
-emp1.AddRtp(5);
-emp1.AddRtp(4);
-
-emp2.AddRtp(3);
-emp2.AddRtp(7);
-emp2.AddRtp(5);
-emp2.AddRtp(9);
-emp2.AddRtp(5);
-
-emp3.AddRtp(1);
-emp3.AddRtp(1);
-emp3.AddRtp(2);
-emp3.AddRtp(8);
-emp3.AddRtp(3);
-
-
-
-int result = emp1.Result;
-int result2 = emp2.Result;
-int result3 = emp3.Result;
-
-Console.WriteLine(emp1.Name + " Wynik " + emp1.Result);
-Console.WriteLine(emp2.Name + " Wynik " + emp2.Result);
-Console.WriteLine(emp3.Name + " Wynik " + emp3.Result);
-
-List<Employee> emps = new List<Employee>()
-
-{
-    emp1, emp2, emp3
-};
-
-
-int maxResult = 0;
-
-Employee empMaxResult = null;
-
-foreach (var emp in emps)
-{
-
-    if (emp.Result > maxResult)
-    {
-        maxResult = emp.Result;
-        empMaxResult = emp;
-    }
-}
-
-Console.WriteLine("====================================");
-Console.WriteLine("    Najlepszy wynik osiąga ");
-Console.WriteLine("     " + empMaxResult.Name + " " + empMaxResult.Surname + " lat " + empMaxResult.Age);
-Console.WriteLine("      Z wynikiem " + maxResult + " pkt ");
-Console.WriteLine("====================================");
